7af0aff72d5af7d1299fccb6439010203047bc4c
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-10-04 21:58:54 -0400

    test

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import src.burgerhash as burgerhash
 
 
 if __name__ == "__main__":
-    result = burgerhash.hash("")
+    result = burgerhash.hash("I am typing a rather long string of words and I don't really know when I'm going to stop I could go on forever really weee!!")
     print(result)
